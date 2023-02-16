0a851d4b6aafc57a67a0f7d8204c64577e470b1f
pogakuramanjan04 <125488490+pogakuramanjan04@users.noreply.github.com>
2023-02-16 16:04:37 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+// CALL APPLLY AND BIND
+const o = {
+  name: "ram"
+};
+function f1() {
+  function f2() {
+    // console.log(this); //
+    function f3() {
+      //   console.log(this); //
+    }
+    f3.call(o); // apply or call to make transfer the context
+  }
+  f2();
+}
+f1();
+
+function ram() {
+  console.log(this, "bind this");
+}
+ram(); // undefined
+
+let bf = ram.bind(o);
+bf();
